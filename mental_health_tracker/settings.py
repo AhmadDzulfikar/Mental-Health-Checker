e07e5413a2cd45a7952658dbb8a71ac8056d133a
AhmadDzulfikar <dzulfikar19.ahmad@gmail.com>
2024-10-01 22:52:16 +0700

    Tampilan applikasi sudah selesai

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -50,6 +50,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', #Tambahkan tepat di bawah SecurityMiddleware
 ]
 
 ROOT_URLCONF = 'mental_health_tracker.urls'
@@ -119,7 +121,13 @@ USE_TZ = True
 # Dokumentasi: https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
+if DEBUG:
+    STATICFILES_DIRS = [
+        BASE_DIR / 'static' # merujuk ke /static root project pada mode development
+    ]
+else:
+    STATIC_ROOT = BASE_DIR / 'static' # merujuk ke /static root project pada mode production
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
